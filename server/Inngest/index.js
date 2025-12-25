566423ba702020ae7c7a533bce2bc09bb7018771
Swapnil <srivastavswapnil007@gmail.com>
2025-12-25 20:13:04 +0530

    correct deletion function

--- server/Inngest/index.js
+++ server/Inngest/index.js
@@ -5,60 +5,55 @@ import prisma from "../configs/prisma.js";
 export const inngest = new Inngest({ id: "project-management-webapplication" });
 
 const syncUserCreation = inngest.createFunction(
-    {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user.created'},
-    async({ event}) => {
-        const {data} = event
+    { id: 'sync-user-from-clerk' },
+    { event: 'clerk/user.created' },
+    async ({ event }) => {
+        const { data } = event;
         await prisma.user.create({
             data: {
                 id: data.id,
                 email: data.email_addresses[0]?.email_address,
-                name: data?.first_name + " " + data?.last_name,
-                image: data?.image_url,
+                name: `${data.first_name} ${data.last_name}`,
+                image: data.image_url,
             }
         })
     }
 )
 
-
-//inngest function to delete the user data in database
-const syncteUserDeletion = inngest.createFunction(
-    {id: 'delete-user-from-clerk'},
-    {event: 'clerk/user.deleted'},
-    async({ event}) => {
-        const {data} = event
+// Inngest function to delete the user data in database
+// FIXED: Renamed function (typo) and fixed Prisma delete syntax
+const syncUserDeletion = inngest.createFunction(
+    { id: 'delete-user-from-clerk' },
+    { event: 'clerk/user.deleted' },
+    async ({ event }) => {
+        const { data } = event;
         await prisma.user.delete({
-            data: {
+            where: { // ✅ Changed from 'data' to 'where'
                 id: data.id,
-
             }
         })
     }
 )
 
-
-//inngest function to update the user data in database
-
-
+// Inngest function to update the user data in database
 const syncUserUpdation = inngest.createFunction(
-    {id: 'update-user-from-clerk'},
-    {event: 'clerk/user.updated'},
-    async({ event}) => {
-        const {data} = event
-        await prisma.user.delete({
+    { id: 'update-user-from-clerk' },
+    { event: 'clerk/user.updated' },
+    async ({ event }) => {
+        const { data } = event;
+        await prisma.user.update({
             where: {
                 id: data.id
             },
             data: {
-                id: data.id,
+                // Note: We do not include 'id' here, as we are already targeting it in 'where'
                 email: data.email_addresses[0]?.email_address,
-                name: data?.first_name + " " + data?.last_name,
-                image: data?.image_url,
+                name: `${data.first_name} ${data.last_name}`,
+                image: data.image_url,
             }
         })
     }
 )
 
-
 // Create an empty array where we'll export future Inngest functions
-export const functions = [syncUserCreation, syncteUserDeletion,syncUserUpdation];
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
